c9fb2b2add914709805cd2662ef970358543a1d3
d1gl8 <d1gl8@yandex.ru>
2024-05-30 15:49:18 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+  // const comments = await $api("/comments", {
+  //   params: { postId: 5 },
+  // });
+  // console.log(comments);
+</script>
+
+<template>
+  <h1>Nuxt 3 Typescript start template</h1>
+</template>
